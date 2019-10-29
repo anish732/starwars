bfa7159a62c2e717445102000fd8388e35e61ccf
Shital Patel <shital732@yahoo.com>
2019-10-29 11:50:59 -0400

    create new folder

---
+++ server.js
@@ -0,0 +1,99 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+//path is a node standard so you don't need to add in dependency..
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3001;
+
+// Sets up the Express app to handle data parsing
+//It is middle wear that mean it is between request and response...need to use it for post
+//app.use means we are using express in middle wear.....
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+  return res.json(characters);
+});
+
+// Displays a single character, or returns false
+app.get("/api/characters/:character", function(req, res) {
+  //If we have space in between name of the chareacter then you might have to +(?) on browser...
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware from line 15 and 16...
+  var newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  //We are adding routeName property in object. We add all other property in add.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
